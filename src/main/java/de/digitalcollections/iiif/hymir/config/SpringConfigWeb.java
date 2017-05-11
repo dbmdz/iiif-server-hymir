e5a0f6901d2282c010710bd92a8f7e60f9c40d26
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-05-11 10:48:33 +0200

    Use global error handling of DC commons spring mvc library

--- src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfigWeb.java
+++ src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfigWeb.java
@@ -1,14 +1,18 @@
 package de.digitalcollections.iiif.hymir.config;
 
 import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import de.digitalcollections.commons.springmvc.config.SpringConfigCommonsMvc;
 import de.digitalcollections.commons.springmvc.interceptors.RequestProcessingTimeInterceptor;
 import java.util.Locale;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.LocaleResolver;
@@ -22,19 +26,22 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.extras.conditionalcomments.dialect.ConditionalCommentsDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration(value = "SpringConfigWebHymir")
 @ComponentScan(basePackages = {
   "de.digitalcollections.iiif.hymir.frontend.impl.springmvc.controller",
-  "de.digitalcollections.commons.springmvc.interceptors" // because of RequestProcessingTimeInterceptor being also an advice...
+  "de.digitalcollections.commons.springmvc.interceptors", // because of RequestProcessingTimeInterceptor being also an advice...
+  "de.digitalcollections.commons.springmvc.controller" // for global exception and error handling
 })
 @EnableAspectJAutoProxy
 @EnableWebMvc
 @PropertySource(value = {
   "classpath:de/digitalcollections/iiif/hymir/config/SpringConfigWeb-${spring.profiles.active:PROD}.properties"
 })
+@Import(SpringConfigCommonsMvc.class)
 public class SpringConfigWeb extends WebMvcConfigurerAdapter {
 
   @Bean
@@ -44,6 +51,10 @@ public class SpringConfigWeb extends WebMvcConfigurerAdapter {
 
   @Value("${cacheTemplates}")
   private boolean cacheTemplates;
+
+  @Autowired
+  @Qualifier("CommonsClasspathThymeleafResolver")
+  private ClassLoaderTemplateResolver commonsClasspathThymeleafResolver;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -65,14 +76,17 @@ public class SpringConfigWeb extends WebMvcConfigurerAdapter {
     templateResolver.setCharacterEncoding("UTF-8");
     templateResolver.setTemplateMode("HTML5");
     templateResolver.setCacheable(cacheTemplates);
-    templateResolver.setOrder(2);
     return templateResolver;
   }
 
   @Bean
   public SpringTemplateEngine templateEngine() {
     SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-    templateEngine.addTemplateResolver(servletContextTemplateResolver());
+    commonsClasspathThymeleafResolver.setOrder(1);
+    final TemplateResolver servletContextTemplateResolver = servletContextTemplateResolver();
+    servletContextTemplateResolver.setOrder(2);
+    templateEngine.addTemplateResolver(commonsClasspathThymeleafResolver);
+    templateEngine.addTemplateResolver(servletContextTemplateResolver);
     // Activate Thymeleaf LayoutDialect[1] (for 'layout'-namespace)
     // [1] https://github.com/ultraq/thymeleaf-layout-dialect
     templateEngine.addDialect(new LayoutDialect());
