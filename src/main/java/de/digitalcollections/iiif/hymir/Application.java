16d0f9dbdfd68b46128734b464e0b83b37390181
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-07-05 17:13:46 +0200

    Add spring actuator endpoints and gui and authentication

--- src/main/java/de/digitalcollections/iiif/hymir/Application.java
+++ src/main/java/de/digitalcollections/iiif/hymir/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
   public static void main(String[] args) throws Exception {
     if (System.getProperty("spring.profiles.active") == null) {
       System.setProperty("spring.profiles.active", "PROD");
