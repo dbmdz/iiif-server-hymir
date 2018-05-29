c7c51660e70c9a574b8e86a058672b034471f99a
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-05-29 14:52:02 +0200

    Add version endpoint, extend info endpoint and fix actuator setup

--- src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfig.java
+++ src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfig.java
@@ -2,11 +2,18 @@ package de.digitalcollections.iiif.hymir.config;
 
 import de.digitalcollections.iiif.model.jackson.IiifObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+@ComponentScan(basePackages = {
+  "de.digitalcollections.commons.springboot.actuator",
+  "de.digitalcollections.commons.springboot.contributor",
+  "de.digitalcollections.commons.springboot.monitoring"
+})
 @Configuration
 public class SpringConfig {
+
   @Bean
   @Primary
   public IiifObjectMapper objectMapper() {
--- src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfigSecurity.java
+++ src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfigSecurity.java
@@ -1,22 +1,38 @@
 package de.digitalcollections.iiif.hymir.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
 
 @Configuration
 public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
+
+  @Value("${spring.security.user.name}")
+  private String actuatorUsername;
+
+  @Value("${spring.security.user.password}")
+  private String actuatorPassword;
+
   @Bean
   public HttpFirewall looseFirewall() {
     DefaultHttpFirewall firewall = new DefaultHttpFirewall();
     firewall.setAllowUrlEncodedSlash(true);
     return firewall;
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().passwordEncoder(passwordEncoderDummy()).withUser(User.withUsername(actuatorUsername).password(actuatorPassword).roles("ACTUATOR"));
   }
 
   @Override
@@ -30,11 +46,25 @@ public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
     http.authorizeRequests()
-            .requestMatchers(EndpointRequest.to("info", "health")).permitAll()
+            .requestMatchers(EndpointRequest.to("health", "info", "version")).permitAll()
             .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
             .antMatchers("/**").permitAll()
             .and()
             .httpBasic();
+  }
+
+  private PasswordEncoder passwordEncoderDummy() {
+    return new PasswordEncoder() {
+      @Override
+      public String encode(CharSequence rawPassword) {
+        return rawPassword.toString();
+      }
+
+      @Override
+      public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return rawPassword.toString().equals(encodedPassword);
+      }
+    };
   }
 
 }
