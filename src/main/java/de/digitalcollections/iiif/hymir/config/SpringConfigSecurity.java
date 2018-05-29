d49c0cc975bc652b878ec19a241af0bf3d6da174
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-05-29 15:33:07 +0200

    Fix static resources

--- src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfigSecurity.java
+++ src/main/java/de/digitalcollections/iiif/hymir/config/SpringConfigSecurity.java
@@ -3,6 +3,7 @@ package de.digitalcollections.iiif.hymir.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -46,6 +47,7 @@ public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
     http.authorizeRequests()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
             .requestMatchers(EndpointRequest.to("health", "info", "version")).permitAll()
             .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
             .antMatchers("/**").permitAll()
