6ff0ad0cda57c93efb2a03a09894fbb3f9554fc4
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-12-06 08:19:36 +0100

    Fix PMD warnings (Use explicit scoping instead of the default package private level)

--- src/test/java/de/digitalcollections/iiif/hymir/config/CustomResponseHeadersTest.java
+++ src/test/java/de/digitalcollections/iiif/hymir/config/CustomResponseHeadersTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CustomResponseHeadersTest {
 
   @Autowired
-  CustomResponseHeaders headers;
+  private CustomResponseHeaders headers;
 
   @Test
   public void testCustomResponseHeadersConfiguration() {
--- src/test/java/de/digitalcollections/iiif/hymir/presentation/frontend/IIIFPresentationApiControllerTest.java
+++ src/test/java/de/digitalcollections/iiif/hymir/presentation/frontend/IIIFPresentationApiControllerTest.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IIIFPresentationApiControllerTest {
 
   @LocalServerPort
-  int randomServerPort;
+  private int randomServerPort;
 
   @Autowired
   protected IIIFPresentationApiController iiifController;
