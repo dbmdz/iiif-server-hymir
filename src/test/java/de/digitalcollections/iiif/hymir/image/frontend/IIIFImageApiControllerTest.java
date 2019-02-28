9f6fc6a0f49584b1353ed927d335f8752fcf708d
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-02-28 11:31:50 +0100

    Bump dc-commons version to 4.0.0-SNAPSHOT and fix corresponding stuff

--- src/test/java/de/digitalcollections/iiif/hymir/image/frontend/IIIFImageApiControllerTest.java
+++ src/test/java/de/digitalcollections/iiif/hymir/image/frontend/IIIFImageApiControllerTest.java
@@ -42,10 +42,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
-        properties = {"spring.profiles.active=TEST",
-                      "spring.config.name=application-customResponseHeaders-test"},
-        classes = {Application.class, TestConfiguration.class},
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+  properties = {"spring.profiles.active=TEST",
+                "spring.config.name=application-test"},
+  classes = {Application.class, TestConfiguration.class},
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IIIFImageApiControllerTest {
 
   @LocalServerPort
