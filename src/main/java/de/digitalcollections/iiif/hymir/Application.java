cd8ef109198a8f8c0dc0f20d564b952a75b4d680
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-03-13 08:15:07 +0100

    Remove CLI stuff

--- src/main/java/de/digitalcollections/iiif/hymir/Application.java
+++ src/main/java/de/digitalcollections/iiif/hymir/Application.java
@@ -1,13 +1,5 @@
 package de.digitalcollections.iiif.hymir;
 
-import de.digitalcollections.iiif.hymir.cli.Cli;
-import de.digitalcollections.iiif.hymir.cli.CliException;
-import de.digitalcollections.iiif.hymir.cli.ExitStatus;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.io.PrintWriter;
-import org.apache.commons.cli.ParseException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,12 +10,9 @@ import org.springframework.core.io.Resource;
 @SpringBootApplication
 public class Application {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
   public static void main(String[] args) {
     // Allow escaped slashes in routes
     System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
-    processArguments(args);
     SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
     builder.banner(hymirBanner());
     builder.run(args);
@@ -32,28 +21,5 @@ public class Application {
   private static Banner hymirBanner() {
     Resource resource = new ClassPathResource("banner.txt");
     return new ResourceBanner(resource);
-  }
-
-  @SuppressFBWarnings(value = "DM_DEFAULT_ENCODING", justification = "The PrintWriter constructor with charset is not supported in Java 8")
-  private static void processArguments(String[] args) {
-    Cli cli;
-    try {
-      cli = new Cli(new PrintWriter(System.out), args);
-      if (cli.hasExitStatus()) {
-        System.exit(cli.getExitStatus());
-      }
-      if (cli.hasRulesPath()) {
-        System.setProperty("multiPatternResolvingFile", cli.getRulesPath());
-      }
-      if (cli.hasSpringProfiles()) {
-        System.setProperty("spring.profiles.active", cli.getSpringProfiles());
-      }
-    } catch (CliException e) {
-      LOGGER.error(e.getMessage());
-      System.exit(ExitStatus.ERROR);
-    } catch (ParseException e) {
-      LOGGER.error("Could not parse command line arguments", e);
-      System.exit(ExitStatus.ERROR);
-    }
   }
 }
