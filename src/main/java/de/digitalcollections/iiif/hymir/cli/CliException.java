eb89afb2e9b04f39e626a233fdf3c4f2ff867d1f
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-03-13 08:15:07 +0100

    Remove CLI stuff

--- src/main/java/de/digitalcollections/iiif/hymir/cli/CliException.java
+++
@@ -1,9 +0,0 @@
-package de.digitalcollections.iiif.hymir.cli;
-
-public class CliException extends Exception {
-
-  public CliException(String message) {
-    super(message);
-  }
-
-}
--- src/main/java/de/digitalcollections/iiif/hymir/cli/ExitStatus.java
+++
@@ -1,9 +0,0 @@
-package de.digitalcollections.iiif.hymir.cli;
-
-public class ExitStatus {
-
-  public static final int OK = 0;
-
-  public static final int ERROR = 1;
-
-}
