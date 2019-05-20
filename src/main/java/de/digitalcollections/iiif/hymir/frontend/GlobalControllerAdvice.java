0716763345debfed64b2c59c2a2f293380bd281a
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-05-20 12:02:40 +0200

    Add webjar versions as global model attribute

---
+++ src/main/java/de/digitalcollections/iiif/hymir/frontend/GlobalControllerAdvice.java
@@ -0,0 +1,20 @@
+package de.digitalcollections.iiif.hymir.frontend;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+  @Autowired
+  @Value("#{webjarVersions}")
+  private Map<String, String> webjarVersions;
+
+  @ModelAttribute("webjarVersions")
+  public Map<String, String> getWebjarVersions() {
+    return webjarVersions;
+  }
+}
